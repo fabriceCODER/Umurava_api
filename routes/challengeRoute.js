9ab01303bd9b52d23e2f20192467898b9c6512fd
FA <fabricecoder009@gmail.com>
2025-02-04 17:30:41 +0200

    fix: call signatures

--- routes/challengeRoute.js
+++ routes/challengeRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const ChallengeController = require("../controllers/challengeController");
+const challengeValidationRules = require("../validations/challengeValidation");
+const validate = require("../middleware/validateMiddleware");
 
 const router = express.Router();
 
 router.get("/", ChallengeController.getAll);
-router.post("/", ChallengeController.create);
+router.post("/", challengeValidationRules, validate, ChallengeController.create);
 router.get("/:id", ChallengeController.getById);
-router.put("/:id", ChallengeController.update);
+router.put("/:id", challengeValidationRules, validate, ChallengeController.update);
 router.delete("/:id", ChallengeController.delete);
 
 module.exports = router;
